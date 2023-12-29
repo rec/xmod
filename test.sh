80782953857a4894932f9fccfce52bbc1ea600ce
Tom Ritchford <tom@swirly.com>
2023-12-29 12:53:50 +0100

    Move xmod.py to xmod/__init__.py

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@
 
 set -eux
 
-mypy xmod.py
-isort xmod.py test
-black xmod.py test
-ruff check --fix xmod.py test
+mypy xmod
+isort xmod test
+black xmod test
+ruff check --fix xmod test
 coverage run $(which pytest)
 coverage html
